ac3d306602c6b2cada1d8e5099a1cc1d67cfb47c
gangagyatso4364 <gyatsokhan@gmail.com>
2023-10-26 17:01:44 +0530

    added pytest

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,7 +2,7 @@ from repo_filter.clone_repo import get_tm_repo_list
 from repo_filter.example import add_one
 
 
-def test_get_tm_repo_list():
+def test_get_tm_repos_list():
     filepath1 = "list1.txt"
     filepath2 = "list2.txt"
     assert get_tm_repo_list(filepath1, filepath2) == [4, 5]
